a65ca1887a3d6d559def7f33d80939ca6e13086d
Michele Formenti <michele.formenti@proton.me>
2026-06-22 21:09:48 +0200

    Created Service and RBAC

---
+++ kodekloud-k8s-challenges/challenge1/setup_user.sh
@@ -0,0 +1,9 @@
+kubectl config set-credentials martin \
+ --client-certificate=/root/martin.crt \
+ --client-key=/root/martin.key
+
+kubectl config set-context developer \
+  --user=martin \
+  --namespace=developer
+
+kubectl config use-context developer
